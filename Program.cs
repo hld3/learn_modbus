b7343809c3079b4622d352f3da1eb6c963153bd2
Harry Dodson <harry.lee.dodson@gmail.com>
2024-09-17 11:50:34 -0400

    confirmed connection to server

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using NModbus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string ipAddress = "ADD SERVER IP";
+            int port = 502;
+
+            TcpClient client = new TcpClient(ipAddress, port);
+
+            var factory = new ModbusFactory();
+            var modbusMaster = factory.CreateMaster(client);
+            byte slaveId = 1;
+            ushort startAddress = 0;
+            ushort numberOfPoints = 10;
+            ushort[] holdingRegisters = modbusMaster.ReadHoldingRegisters(slaveId, startAddress, numberOfPoints);
+            Console.WriteLine("Holding Registers:");
+            for (int i = 0; i < holdingRegisters.Length; i++)
+            {
+                Console.WriteLine($"Address {startAddress + i}: {holdingRegisters[i]}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
